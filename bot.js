77c69bb47d79daac4d362d697e71623646483ee0
Dopaminergics <josephmctigue@gmail.com>
2021-11-22 19:53:45 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,18 +58,12 @@ let daiContractSetup = false;
 // 3. INIT: CHECK ENV VARS 
 // --------------------------------------------
 console.log("-------------------------------------------------------------------------------------------------------------")
-console.log("|              Welcome to the SHOSHIN-gCUBE unofficial b-cube.ai signalz for gains.trade                    |");
+console.log("|              				初心 gainsCUBE 初心                                               |")
+console.log("|              	      初心 Unofficial b-cube.ai signalz for gains.trade 初心                              |");
+console.log("|-----------------------------------------------------------------------------------------------------------|")
 console.log("|                     BETA:   PLEASE MONITOR THE BOT AND POSITIONS AT THIS STAGE.                           |")
 console.log("|             Please leave dev_fee as 1% or greater. Signals worth > 350 EUR/mo. Consider 2%.               |")
 console.log("|-----------------------------------------------------------------------------------------------------------|")
-console.log("|  - BETA: ENSURE ADDRESS IS APPROVED FOR DAI ON GAINS.TRADE. THIS WILL BE BUILT IN FOR FULL RELEASE.       |")
-console.log("|  - The server runs on heroku - during beta any downtime will result in no further positons being opened.  |")
-console.log("|  - Currently signals should only open for SOL and XRP                                                     |")
-console.log("|  - Refer to BCUBE Website: SOL Positional Bot.                                                            |")
-console.log("|  - Refer to BCUBE Website: XRP Short Term Bot.                                                            |")
-console.log("|  - Default dev_fee 2%                                                                                     |")
-console.log("|  - Review your capital and percentage to use per position in the .env.                                    |")
-console.log("-------------------------------------------------------------------------------------------------------------")
 if(!process.env.WSS_URLS || !process.env.PRICES_URL || !process.env.STORAGE_ADDRESS
 || !process.env.PUBLIC_KEY || !process.env.EVENT_CONFIRMATIONS_SEC 
 || !process.env.TRIGGER_TIMEOUT || !process.env.GAS_PRICE_GWEI || !process.env.CHECK_REFILL_SEC
